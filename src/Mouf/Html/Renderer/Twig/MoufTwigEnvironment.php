acd4d97d8da298a6876c4fe4bf7c216c00b39fa2
David Négrier <d.negrier@thecodingmachine.com>
2013-09-19 18:18:42 +0200

    Adding twig template and twig environment class

---
+++ src/Mouf/Html/Renderer/Twig/MoufTwigEnvironment.php
@@ -0,0 +1,63 @@
+<?php
+namespace Mouf\Html\Renderer\Twig;
+
+use \Twig_LoaderInterface;
+use \Twig_ExtensionInterface;
+use Mouf\MoufManager;
+
+/**
+ * The simple class extending the Twig_Environment class whose sole purpose is to make it
+ * easy to work with the Twig_Environement class in Mouf.
+ * 
+ * It does this by overloading the constructor and adding a bunch of parameters that would
+ * otherwise be passed in the very inconvenient config array.
+ * 
+ * It also automatically registers the MoufTwigExtension extension.
+ * 
+ * @author David Negrier <david@mouf-php.com>
+ */
+class MoufTwigEnvironment extends \Twig_Environment {
+
+	/**
+	 * 
+	 * @param Twig_LoaderInterface $loader The loader used by Twig. If null, the Twig_Loader_Filesystem is used, and is relative to ROOT_PATH.
+	 * @param array<string, string> $options
+	 * @param string $cacheDirectory Relative to ROOT_PATH, unless null. In this case will be generated in the sys temporary directory.
+	 * @param bool $autoReload Whether we should autoreload the environment or not.
+	 */
+	public function __construct(Twig_LoaderInterface $loader = null, $options = array(), 
+			$cacheDirectory = null, $autoReload = true) {
+		if ($loader == null) {
+			$loader = new \Twig_Loader_Filesystem(ROOT_PATH);
+		}
+		
+		if ($cacheDirectory) {
+			$cacheDirectory = ROOT_PATH.ltrim($cacheDirectory,'\\/');
+		} else {
+			$cacheDirectory = rtrim(sys_get_temp_dir().'/\\').'/mouftwigtemplatemain_'.str_replace(":", "", ROOT_PATH);
+		}
+		
+		$addtionnalOptions = array(
+			// The cache directory is in the temporary directory and reproduces the path to the directory (to avoid cache conflict between apps).
+			'cache' => $cacheDirectory,
+			'auto_reload' => $autoReload
+		);
+		
+		$options = array_merge($addtionnalOptions, $options);
+		
+		parent::__construct($loader, $options);
+		
+		$this->addExtension(new MoufTwigExtension(MoufManager::getMoufManager()));
+	}
+	
+	/**
+	 * Registers an array of extensions.
+	 * Note: the sole purpose of this function is to overload the @param annotation.
+	 *
+	 * @param Twig_ExtensionInterface[] $extensions An array of extensions
+	 */
+	public function setExtensions(array $extensions)
+	{
+		parent::setExtensions($extensions);
+	}
+}
--- src/install.php
+++ src/install.php
@@ -14,28 +14,10 @@ use Mouf\Html\Renderer\ChainableRendererInterface;
 // Let's init Mouf
 InstallUtils::init(InstallUtils::$INIT_APP);
 
-// Let's create the instance
 $moufManager = MoufManager::getMoufManager();
 
-$rendererCacheService = InstallUtils::getOrCreateInstance("rendererCacheService", "Mouf\\Utils\\Cache\\InMemoryCache", $moufManager);
-if ($moufManager->instanceExists("apcCacheService")) {
-	$rendererCacheService->getProperty("chainWith")->setValue($moufManager->getInstanceDescriptor("apcCacheService"));
-}
-
-$customRenderer = InstallUtils::getOrCreateInstance("customRenderer", "Mouf\\Html\\Renderer\\FileBasedRenderer", $moufManager);
-$customRenderer->getProperty("directory")->setValue("src/templates");
-$customRenderer->getProperty("cacheService")->setValue($rendererCacheService);
-$customRenderer->getProperty("type")->setValue(ChainableRendererInterface::TYPE_CUSTOM);
-$customRenderer->getProperty("priority")->setValue(0);
-
-$defaultRenderer = InstallUtils::getOrCreateInstance("defaultRenderer", "Mouf\\Html\\Renderer\\AutoChainRenderer", $moufManager);
-$defaultRenderer->getProperty("cacheService")->setValue($rendererCacheService);
-
-if (!file_exists(ROOT_PATH.'src/templates')) {
-	$old = umask(0);
-	mkdir(ROOT_PATH.'src/templates', 0775, true);
-	umask($old);
-}
+// Let's create the instances.
+$twigEnvironment = InstallUtils::getOrCreateInstance('twigEnvironment', 'Mouf\\Html\\Renderer\\Twig\\MoufTwigEnvironment', $moufManager);
 
 // Let's rewrite the MoufComponents.php file to save the component
 $moufManager->rewriteMouf();
