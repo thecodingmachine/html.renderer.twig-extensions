fbe94b01bcd2a349a5acf7cb533e2440c0c491f1
David Négrier <d.negrier@thecodingmachine.com>
2015-10-08 19:18:49 +0200

    Fixing installer

--- src/Mouf/Html/Renderer/Twig/MoufTwigEnvironmentInstaller2.php
+++ src/Mouf/Html/Renderer/Twig/MoufTwigEnvironmentInstaller2.php
@@ -20,19 +20,22 @@ class MoufTwigEnvironmentInstaller2 implements PackageInstallerInterface
     public static function install(MoufManager $moufManager)
     {
         // Let's create the instances.
-        $twigEnvironment = InstallUtils::getOrCreateInstance('twigEnvironment', 'Mouf\\Html\\Renderer\\Twig\\MoufTwigEnvironment', $moufManager);
-        $moufTwigExtension = InstallUtils::getOrCreateInstance('moufTwigExtension', 'Mouf\\Html\\Renderer\\Twig\\MoufTwigExtension', $moufManager);
-        $twigDebugExtension = InstallUtils::getOrCreateInstance('twigDebugExtension', 'Twig_Extension_Debug', $moufManager);
-        $twigCacheFileSystem = InstallUtils::getOrCreateInstance('twigCacheFileSystem', 'Twig_Cache_Filesystem', $moufManager);
         $twigLoaderFileSystem = InstallUtils::getOrCreateInstance('twigLoaderFileSystem', 'Twig_Loader_Filesystem', $moufManager);
+        $twigEnvironment = InstallUtils::getOrCreateInstance('twigEnvironment', 'Mouf\\Html\\Renderer\\Twig\\MoufTwigEnvironment', $moufManager);
+        $twigDebugExtension = InstallUtils::getOrCreateInstance('twigDebugExtension', 'Twig_Extension_Debug', $moufManager);
+        $moufTwigExtension = InstallUtils::getOrCreateInstance('moufTwigExtension', 'Mouf\\Html\\Renderer\\Twig\\MoufTwigExtension', $moufManager);
+        $twigCacheFileSystem = InstallUtils::getOrCreateInstance('twigCacheFileSystem', 'Twig_Cache_Filesystem', $moufManager);
 
         // Let's bind instances together.
-        if (!$twigEnvironment->getConstructorArgumentProperty('loader')->isValueSet()) {
-            $twigEnvironment->getConstructorArgumentProperty('loader')->setValue($twigLoaderFileSystem);
-        }
-        if (!$twigEnvironment->getConstructorArgumentProperty('options')->isValueSet()) {
-            $twigEnvironment->getConstructorArgumentProperty('options')->setValue('return array(\'debug\' => DEBUG, \'auto_reload\' => true);');
-        }
+        $twigLoaderFileSystem->getConstructorArgumentProperty('paths')->setValue('return ROOT_PATH;');
+        $twigLoaderFileSystem->getConstructorArgumentProperty('paths')->setOrigin("php");
+        
+        $twigEnvironment->getConstructorArgumentProperty('loader')->setValue($twigLoaderFileSystem);
+
+
+        $twigEnvironment->getConstructorArgumentProperty('options')->setValue('return array(\'debug\' => DEBUG, \'auto_reload\' => true);');
+        $twigEnvironment->getConstructorArgumentProperty('options')->setOrigin("php");
+
         if (!$twigEnvironment->getSetterProperty('setExtensions')->isValueSet()) {
             $twigEnvironment->getSetterProperty('setExtensions')->setValue(array(0 => $moufTwigExtension, 1 => $twigDebugExtension, ));
         }
@@ -41,6 +44,7 @@ class MoufTwigEnvironmentInstaller2 implements PackageInstallerInterface
         }
         if (!$moufTwigExtension->getConstructorArgumentProperty('container')->isValueSet()) {
             $moufTwigExtension->getConstructorArgumentProperty('container')->setValue('return $container;');
+            $moufTwigExtension->getConstructorArgumentProperty('container')->setOrigin("php");
         }
         if (!$twigCacheFileSystem->getConstructorArgumentProperty('directory')->isValueSet()) {
             $twigCacheFileSystem->getConstructorArgumentProperty('directory')->setValue('// If we are running on a Unix environment, let\'s prepend the cache with the user id of the PHP process.
@@ -52,11 +56,6 @@ if (function_exists(\'posix_geteuid\')) {
 }
 return rtrim(sys_get_temp_dir(), \'/\\\\\').\'/mouftwigtemplatemain_\'.$posixGetuid.str_replace(":", "", ROOT_PATH);');
         }
-        if (!$twigLoaderFileSystem->getConstructorArgumentProperty('paths')->isValueSet()) {
-            $twigLoaderFileSystem->getConstructorArgumentProperty('paths')->setValue('return ROOT_PATH;');
-        }
-
-
 
         // Let's rewrite the MoufComponents.php file to save the component
         $moufManager->rewriteMouf();
